d25025334facde2cd085f49543ba2dc6ace1cb33
mutelnwza <mutemallow@gmail.com>
2026-03-08 14:13:51 +0700

    add prefix

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        String[]arr = {"5","+","2","/","2","-","1"};
+        String[]arr2= {"1","+","2","*","3","/","4"};
+        ArrayList<String> a = new ArrayList<>(Arrays.asList(arr));
+        ArrayList<String> a2 = new ArrayList<>(Arrays.asList(arr2));
+        Prefix p = new Prefix(a2);
+        p.show();
+    }
+}
